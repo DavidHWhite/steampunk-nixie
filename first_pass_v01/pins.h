ee3b1ea76c8862231d1fac34b392ad65689b56ea
David White <david@davidwhite.bio>
2023-11-30 20:58:29 -0500

    Update pins.h

--- first_pass_v01/pins.h
+++ first_pass_v01/pins.h
@@ -3,33 +3,56 @@
 
 namespace pins {
   namespace cathode {
-    constexpr int DATA    = 2,
-                  SCLK    = 3,
-                  LATCH   = 4,
-                  DISABLE = 5;
+    constexpr int DATA    =  2,
+                  SCLK    =  3,
+                  LATCH   =  6,
+                  DISABLE =  7;
   }
   
   namespace anode {
-    constexpr int DATA    = 6,
-                  SCLK    = 7,
-                  LATCH   = 8,
-                  DISABLE = 9;
+    constexpr int DATA    =  8,
+                  SCLK    =  9,
+                  LATCH   = 10,
+                  DISABLE = 11;
   }
   
   namespace rtc {
+    // Not currently planning to use O32K but I'm connecting it just in case
     constexpr int SDA  = 18, // Unsure if these first two need to be specified since they're the only pins available for I2C
                   SCL  = 19,
-                  SQW  = 20,
-                  O32K = 21; // Not currently planning to use this, but connecting just in case
+                  INT  = 15, // Full async detection for interrupts!
+                  O32K = 20;
   }
   
   namespace input {
-    constexpr int HOUR_INC = 10,
-                  HOUR_DEC = 11,
-                  MIN_INC  = 12,
-                  MIN_DEC  = 14, // Skipping 13 so I don't have to work around the built-in LED
-                  HOUR_FORMAT_SWITCH = 15;
+    /*
+      IMPORTANT NOTE ABOUT INTERRUPTS WHILE THE CHIP IS ASLEEP:
+      Only a few pins support full asynchronous interrupt detection, see I/O Multiplexing and Considerations.
+      These limitations apply for waking the system from sleep:
+        tl;dr only the fully async pins can wake on a rising or falling edge rather than just "change" or "level"
+      Pins Px2 and Px6 of each port have full asynchronous detection.
+        This only matches a few pins on the Arduino pinout:
+        - D4 (PC6), D5 (PB2), and D15 (PD2) are all fully available (3 pins)
+        - D13 (PE2) is attached to the built-in LED but could theoretically also be used
+        - D18 (PA2/PF2) is also SDA and thus will be used for the RTC
+      
+      Since there aren't 5 fully async pins available, I'll put the format switch on an async pin
+      and then I'll connect a diode OR of the four buttons to another async pin. That way I don't
+      need any interrupts set up for the individual buttons.
+        Oh and rtc::SQW also needs an async pin.
+
+      Note that these interrupts (with the exception of SQW) are EXCLUSIVELY being used
+      to wake the board from sleep. They aren't being used to set any state
+    */
+    constexpr int HOUR_INC           = 12,
+                  HOUR_DEC           = 14,
+                  MIN_INC            = 16,
+                  MIN_DEC            = 17,
+                  BUTTON_OR          =  4, // Full async detection for interrupts!
+                  HOUR_FORMAT_SWITCH =  5; // Full async detection for interrupts!
   }
 }
+
+// I'm avoiding pin 13 just so I don't have to deal with the built-in LED
 
 #endif
