c2387642e4326a38c9bf7e4745b1240c96181186
David White <david@davidwhite.bio>
2023-12-04 12:02:04 -0500

    Update RTC outline

--- first_pass_v01/rtc.cpp
+++ first_pass_v01/rtc.cpp
@@ -2,6 +2,7 @@
 #include <DS3231.h>
 
 #include "rtc.h"
+#include "pins.h"
 
 // TODO in setup(), implement this safeguard to stop Alarm 2 from misbehaving:
 // https://github.com/NorthernWidget/DS3231/blob/master/Documentation/Alarms.md#how-and-why-to-prevent-an-alarm-entirely
@@ -11,7 +12,24 @@
 // 0b00001110 in alarmBits will setup Alarm 1 to trigger every minute (must set seconds to 0)
 
 namespace rtc {
+  static bool hasMinutePassed = true;
+
+  /*
+   * ISR to allow the buttons and switch to wake the MCU from sleep
+   */
+
+  void rtc_isr() {
+    hasMinutePassed = true;
+  }
+
+  /*
+   * Publicly accessible RTC operations declared in header
+   */
+
   void setup() {
+    // TODO set pin modes
+    attachInterrupt(digitalPinToInterrupt(pins::rtc::INT), rtc_isr, RISING);
+
     // TODO implement
   }
 
@@ -25,5 +43,13 @@ namespace rtc {
 
   void get_time(int* hour, int* minute, bool* isTwentyFourHour) {
     // TODO implement
+  }
+
+  bool has_minute_passed() {
+    return hasMinutePassed;
+  }
+
+  void reset_minute_passed() {
+    hasMinutePassed = false;
   }
 }
--- first_pass_v01/rtc.h
+++ first_pass_v01/rtc.h
@@ -10,6 +10,8 @@ namespace rtc {
   void apply_time_delta(int hour, int minute); // Resets seconds if minute != 0
   void set_hour_mode(HourMode mode);
   void get_time(int* hour, int* minute, bool* isTwentyFourHour);
+  bool has_minute_passed();
+  void reset_minute_passed();
 }
 
 #endif
